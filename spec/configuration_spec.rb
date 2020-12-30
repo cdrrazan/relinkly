a7caf2fdd10d15cecb68630b2e94d7246ce0339d
Rajan Bhattarai <cdrrazan@gmail.com>
2020-12-30 20:36:11 +0545

    Fix existing specs

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,19 +1,17 @@
 require "spec_helper"
 
-describe Relinkly do
-  describe Configuration do
-    describe '#api_key' do
-      it 'has a default value of nil' do
-        Configuration.new.api_key = nil
-      end
+describe Relinkly::Configuration do
+  describe '#api_key' do
+    it 'has a default value of nil' do
+      Relinkly::Configuration.new.api_key = nil
     end
+  end
 
-    describe '#api_key=' do
-      it 'can set value' do
-        config = Configuration.new
-        config.api_key = '123456789'
-        expect(config.api_key).to eq '123456789'
-      end
+  describe '#api_key=' do
+    it 'can set value' do
+      config = Relinkly::Configuration.new
+      config.api_key = '123456789'
+      expect(config.api_key).to eq '123456789'
     end
   end
 end
